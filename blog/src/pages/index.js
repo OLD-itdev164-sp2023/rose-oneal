2ffa15ce773b6066e55d517a9b117c17459c9948
onealr4 <onealr4@gmatc.matc.edu>
2023-02-23 18:53:17 -0600

    Completed Activity 2

--- blog/src/pages/index.js
+++ blog/src/pages/index.js
@@ -1,12 +1,11 @@
 import * as React from "react"
 import { graphql, Link } from "gatsby"
-import { GatsbyImage } from "gatsby-plugin-image"
 
 import Layout from "../components/layout"
 import Seo from "../components/seo"
 import * as styles from "../components/index.module.css"
 
-const IndexPage = () => (
+const IndexPage = ({ data }) => (
   <Layout>
     <Seo title="Home" />
     <ul className={styles.list}>
@@ -14,14 +13,6 @@ const IndexPage = () => (
         data.allContentfulBlogPost.edges.map(edge => (
           <li key={edge.node.id}>
             <Link to={edge.node.slug}>{edge.node.title}</Link>
-            <div>
-              <GatsbyImage
-              image={edge.node.heroImage.gatsbyImageData}
-              />
-            </div>
-            <div>
-              {edge.node.body.childMarkdownRemark.excerpt}
-            </div>
           </li>
         ))
 
@@ -47,18 +38,6 @@ export const query = graphql`
           id
           title
           slug
-          body {
-            childMarkdownRemark {
-              excerpt
-            }
-          }
-          heroImage {
-            gatsbyImageData{
-              layout: CONSTRAINED
-              placeholder: BLURRED
-              width: 300
-            }
-          }
         }
       }
     }
