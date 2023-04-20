662ac1b914623b82a6a9d1d267ae65272a4ebeac
onealr4 <onealr4@gmatc.matc.edu>
2023-04-20 17:12:26 -0500

    List

--- blog/src/components/BaseContainer/BaseContainer.js
+++ blog/src/components/BaseContainer/BaseContainer.js
@@ -1,6 +1,6 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import { Flex, Box } from 'rebass'
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Flex, Box } from 'rebass';
 
 export const BaseContainer = ({ childer, flex = false, ...rest }) => {
     let Component = flex ? Flex : Box
